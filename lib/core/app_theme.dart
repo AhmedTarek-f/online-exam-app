cf8c108d5bbe7659593c264618b3af2875712cdb
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-30 22:32:05 +0300

    adding app theme

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:online_exam_app/core/constants/app_colors.dart';
+
+abstract class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    scaffoldBackgroundColor: AppColors.white,
+    fontFamily: "Inter",
+    colorScheme: ColorScheme.fromSeed(
+      primary: AppColors.primary,
+      secondary: AppColors.white,
+      onSecondary: AppColors.black,
+      shadow: AppColors.gray,
+      surface: AppColors.lightGray,
+      primaryContainer: AppColors.red,
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.light,
+    ),
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w500,
+        color: AppColors.black,
+      ),
+      labelMedium: TextStyle(
+        fontSize: 18.sp,
+        fontWeight: FontWeight.w500,
+        color: AppColors.black,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w500,
+        color: AppColors.white,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14.sp,
+        fontWeight: FontWeight.w400,
+        color: AppColors.lightGray,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 13.sp,
+        fontWeight: FontWeight.w400,
+        color: AppColors.black,
+      ),
+      labelSmall: TextStyle(
+        fontSize: 12.sp,
+        fontWeight: FontWeight.w400,
+        color: AppColors.red,
+      ),
+    ),
+    appBarTheme: AppBarTheme(
+      scrolledUnderElevation: 0,
+      backgroundColor: AppColors.white,
+      titleTextStyle: TextStyle(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w500,
+        color: AppColors.black,
+      ),
+      centerTitle: false,
+      surfaceTintColor: AppColors.black,
+    ),
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: AppColors.primary,
+      selectionHandleColor: AppColors.primary,
+      selectionColor: AppColors.primary.withValues(alpha: 0.2),
+    ),
+  );
+}
