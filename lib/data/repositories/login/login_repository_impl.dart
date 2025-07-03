1eb1260a6e6a501cec519ec87caad57ee1862331
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-07-03 22:01:48 +0300

    renamed path

--- lib/data/repositories/login/login_repository_impl.dart
+++ lib/data/repositories/login/login_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:injectable/injectable.dart';
 import 'package:online_exam_app/data/data_source/login/remote_data_source/login_remote_data_source.dart';
 import 'package:online_exam_app/domain/entities/login/user_login_entity.dart';
-import 'package:online_exam_app/domain/repositories/login_repository.dart';
+import 'package:online_exam_app/domain/repositories/login/login_repository.dart';
 
 @Injectable(as: LoginRepository)
 class LoginRepositoryImpl implements LoginRepository {
