7438cbe38395b665b9d41b5ec334667ab0f3b4b7
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-07-03 21:49:49 +0300

    adding forget password email state

---
+++ lib/presentation/forget_password/forget_password_email/views_model/forget_password_email_state.dart
@@ -0,0 +1,20 @@
+import 'package:online_exam_app/core/utils/exceptions/response_exception.dart';
+
+sealed class ForgetPasswordEmailState {}
+
+final class ForgetPasswordEmailInitial extends ForgetPasswordEmailState {}
+
+final class EnableAutoValidateModeState extends ForgetPasswordEmailState {}
+
+final class SendEmailVerificationSuccessState extends ForgetPasswordEmailState {
+  SendEmailVerificationSuccessState({required this.info});
+  final String? info;
+}
+
+final class SendEmailVerificationFailureState extends ForgetPasswordEmailState {
+  SendEmailVerificationFailureState({required this.errorData});
+  final ResponseException errorData;
+}
+
+final class SendEmailVerificationLoadingState
+    extends ForgetPasswordEmailState {}
